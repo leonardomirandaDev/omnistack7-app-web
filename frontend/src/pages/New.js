0b0b1c2518593dbe23817e85179d67b7d15c1fc2
leoamiranda2 <leoamiranda2@gmail.com>
2019-06-13 17:41:28 -0300

    Pagina de cadastro de post completa

--- frontend/src/pages/New.js
+++ frontend/src/pages/New.js
@@ -1,9 +1,85 @@
 import React, { Component } from 'react';
+import api from '../services/api';
+// css
+import './New.css';
+
 
 export default class New extends Component {
+    state = {
+        image : null,
+        author : '',
+        place : '',
+        description : '',
+        hashtags : ''
+    };
+
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleImageChange = e => {
+        this.setState({
+            image : e.target.files[0]
+        })
+    }
+
+    handleSubmit = async e => {
+        e.preventDefault();
+
+        const data = new FormData();
+
+        data.append('image', this.state.image);
+        data.append('author', this.state.author);
+        data.append('place', this.state.place);
+        data.append('description', this.state.description);
+        data.append('hashtags', this.state.hashtags);
+
+        await api.post('posts', data);
+
+        this.props.history.push('/');
+    }
+
     render() {
         return (
-            <h1>New</h1>
+            <form id="new-post" onSubmit={this.handleSubmit} >
+                <input 
+                    type="file"
+                    onChange={this.handleImageChange}
+                />
+                <input 
+                    type="text"
+                    name="author"
+                    onChange={this.handleChange}
+                    value={this.state.author}
+                    placeholder="Autor do post"
+                />
+
+                <input 
+                    type="text"
+                    name="place"
+                    onChange={this.handleChange}
+                    value={this.state.place}
+                    placeholder="Local do post"
+                />
+                <input 
+                    type="text"
+                    name="description"
+                    onChange={this.handleChange}
+                    value={this.state.description}
+                    placeholder="Descrição do post"
+                />
+                <input 
+                    type="text"
+                    name="hashtags"
+                    onChange={this.handleChange}
+                    value={this.state.hashtags}
+                    placeholder="Hashtags do post"
+                />
+
+                <button type="submit">Enviar</button>
+            </form>
         );
     }
 }
