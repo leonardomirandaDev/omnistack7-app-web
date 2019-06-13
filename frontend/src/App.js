4e364395418fac273488635d15fa66c354c2723a
leoamiranda2 <leoamiranda2@gmail.com>
2019-06-13 10:50:58 -0300

    instalação e configuração de rotas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import Routes from './routes';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Routes/>
+    </BrowserRouter>
   );
 }
-
-export default App;
